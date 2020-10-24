2f6c1c0fb29bbc73d41d3ff5a3469319ae523d94
Ryunosuke Muramatsu <riscait@gmail.com>
2020-10-24 19:35:07 +0900

    Append Widget Preview and comments

--- GraphidgetWidget/GraphidgetWidget.swift
+++ GraphidgetWidget/GraphidgetWidget.swift
@@ -10,15 +10,18 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    // 初回表示に呼ばれるが、呼ばれないこともあるようだ
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
+    // Widgetの追加時に呼ばれる
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
+    // 1. Widgetをホーム画面に表示する時 2. ユーザー入力(configuration)を受け付けた後 に呼ばれる
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
@@ -35,11 +38,13 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
+// 更新のタイミングでWidgetに渡すデータの定義
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
 }
 
+// Widgetに表示するView
 struct GraphidgetWidgetEntryView : View {
     var entry: Provider.Entry
 
@@ -48,6 +53,7 @@ struct GraphidgetWidgetEntryView : View {
     }
 }
 
+// エントリーポイント
 @main
 struct GraphidgetWidget: Widget {
     let kind: String = "GraphidgetWidget"
@@ -56,14 +62,19 @@ struct GraphidgetWidget: Widget {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             GraphidgetWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        // Widget名
+        .configurationDisplayName("Chart")
+        // Widgetの説明
+        .description("This is an chart.")
     }
 }
 
+// プレビューに定義
 struct GraphidgetWidget_Previews: PreviewProvider {
     static var previews: some View {
         GraphidgetWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+        GraphidgetWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
 }
